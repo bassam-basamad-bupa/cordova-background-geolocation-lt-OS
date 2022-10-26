01a3a1499696d05f59e2877f0e21082b58e4dada
André Grillo <andregrillo@gmail.com>
2022-10-26 22:06:37 +0100

    Cleaning old pods

--- hooks/adjust_podfile.js
+++ hooks/adjust_podfile.js
@@ -2,10 +2,6 @@ var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
 
-function run_pod_install(context){
-
-}
-
 module.exports = function(context) {
 
     const projectRoot = context.opts.projectRoot;
@@ -15,21 +11,29 @@ module.exports = function(context) {
         var target2 = 'PureeOS';
         
         fs.readFile(podfilePath, {encoding: 'utf-8'}, function(err, data) {
+
+            //Clean Pods
+            var pathiOS = path.join(context.opts.projectRoot,"platforms","ios");
+            var child = child_process.execSync('rm -rf Pods;rm -rf Podfile.lock;pod cache clean --all', {cwd:pathiOS});
+            console.log("⭐️ Pod Cleaning: Process finished ⭐️");
+            if(child.error) {
+                console.log("🚨 ERROR cleaning pods: ",child.error);
+            }
+
             if (err) throw error;
 
             let dataArray = data.split('\n');
 
             for (let index=0; index<dataArray.length; index++) {
                 if (dataArray[index].includes(target1)) {
-                    dataArray.splice(index, 1, "\tpod 'CocoaLumberjack', '~> 3.7.2'");
+                    dataArray.splice(index, 1, "\tpod 'CocoaLumberjack', '~> 3.7.2'\n\tpod 'YapDatabase',  :git => 'https://github.com/andregrillo/YapDatabase.git', :branch => '2.9-OS'");
                     break; 
                 }
             }
 
             for (let index=0; index<dataArray.length; index++) {
                 if (dataArray[index].includes(target2)) {
-                    //dataArray.splice(index, 1, "\tpod 'PureeOS',  :git => 'https://github.com/andregrillo/puree-ios.git', :branch => '2.0.1.OS6.1'");
-                    dataArray.splice(index, 1);
+                    dataArray.splice(index, 1, "\tpod 'PureeOS',  :git => 'https://github.com/andregrillo/puree-ios.git', :branch => '2.0.1.OS6.1'");
                     break; 
                 }
             }
